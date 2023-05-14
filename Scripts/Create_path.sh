101edd096457c15c0cdd3eac27e5aa651e7c4eef
Maik Wolfram-Schauerte <maik.schauerte@web.de>
2023-05-14 20:02:48 +0000

    To create the required folder structure for CRISPRT4

---
+++ Scripts/Create_path.sh
@@ -0,0 +1,11 @@
+mkdir CRISPRT4
+cd CRISPRT4
+mkdir input
+mkdir input/fastq
+mkdir input/reference
+mkdir output
+mkdir output/alignment
+mkdir output/vcfFiles
+mkdir output/countData 
+mkdir Demultiplexing
+ 
