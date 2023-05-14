d03bf841c111c34c1500c1b05675ba78c490ffc9
Maik Wolfram-Schauerte <maik.schauerte@web.de>
2023-05-14 19:59:36 +0000

    For variant calling and featureCounts with M1 chip

---
+++ Scripts/Mapping_M1.sh
@@ -0,0 +1,21 @@
+# Adjust this single line of code to specify the absolute path to the reference genome for mapping and variant calling
+REFERENCE="/Users/maikschauerte/Data-Analysis/NP_final/ONTrun_079/CRISPRT4/input/reference/NC_000866-4.fasta"
+GFF3="/Users/maikschauerte/Data-Analysis/NP_final/ONTrun_079/CRISPRT4/input/reference/NC_000866-4.gff3"
+
+
+cd input/fastq
+for FILE in *.fastq
+do
+SAMFILE="${FILE/%.fastq/.sam}"
+BAMFILE="${FILE/%.fastq/.bam}"
+BAMFILESORTED="${FILE/%.fastq/_sorted.bam}"
+minimap2 -ax map-ont $REFERENCE $FILE > $SAMFILE
+samtools view -S -b $SAMFILE > $BAMFILE
+samtools sort $BAMFILE -o $BAMFILESORTED
+samtools index $BAMFILESORTED
+done
+mv *.bam *.sam *.bai ..
+cd .. 
+mv *.bam *.sam *.bai ..
+cd .. 
+mv *.bam *.sam *.bai output/alignment
