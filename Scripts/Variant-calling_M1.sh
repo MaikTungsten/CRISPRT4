141d067f345c92c1b69f3daa38099518d711452e
Maik Wolfram-Schauerte <maik.schauerte@web.de>
2023-06-12 18:42:50 +0000

    Update Variant-calling_M1.sh

--- Scripts/Variant-calling_M1.sh
+++ Scripts/Variant-calling_M1.sh
@@ -1,15 +1,15 @@
 # Adjust these lines of code to specify the absolute path to the reference genome for mapping and variant calling
-REFERENCE="/Users/maikschauerte/Data-Analysis/NP_final/ONTrun_079/CRISPRT4/input/reference/NC_000866-4.fasta"
-GFF3="/Users/maikschauerte/Data-Analysis/NP_final/ONTrun_079/CRISPRT4/input/reference/NC_000866-4.gff3"
+REFERENCE="/Users/.../ONTrun_079/CRISPRT4/input/reference/NC_000866-4.fasta"
+GFF3="/Users/.../ONTrun_079/CRISPRT4/input/reference/NC_000866-4.gff3"
 
-# Specify the target region for variant calling. This is a string combining the reference genome id and the region indicated by numbers, for instance the following region for alt gene
+# Specify the target region for variant calling. This is a string combining the reference genome id and the region indicated by numbers, for instance the following region for modA gene
 TargetRegion="NC_000866.4:11908-12510"
 
 cd output/alignment
 for FILE in *sorted.bam
 do
 VCFFILE="${FILE/%_sorted.bam/_variants.vcf}"
-longshot --bam $FILE --ref $REFERENCE --out $VCFFILE --max_cov 120000 -F -r $TargetRegion
+longshot --bam $FILE --ref $REFERENCE --out $VCFFILE --max_cov 120000 -F -x -E 0.6 -r $TargetRegion
 done
 
 mv *.vcf ..
