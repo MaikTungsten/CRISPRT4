f7c0d58b22ee96ece0a6c6e21aefbad9725b6f7d
Maik Wolfram-Schauerte <maik.schauerte@web.de>
2023-09-29 14:21:49 +0000

    Update Mapping_M1.sh

--- Scripts/Mapping_M1.sh
+++ Scripts/Mapping_M1.sh
@@ -1,6 +1,6 @@
 # Adjust these lines of code to specify the absolute path to the reference genome for mapping and variant calling
-REFERENCE="/Users/.../CRISPRT4/input/reference/NC_000866-4.fasta"
-GFF3="/Users/.../CRISPRT4/input/reference/NC_000866-4.gff3"
+REFERENCE="/Users/example_user/CRISPRT4/input/reference/NC_000866-4.fasta"
+GFF3="/Users/example_user/CRISPRT4/input/reference/NC_000866-4.gff3"
 
 
 cd input/fastq
